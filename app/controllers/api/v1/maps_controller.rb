1d4c285559850d3fc710fce0395d1ab169f09684
Lennon Manchester <le.manchester@gmail.com>
2015-10-07 23:30:19 -0300

    Add show to maps controller and specs

--- app/controllers/api/v1/maps_controller.rb
+++ app/controllers/api/v1/maps_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::MapsController < ApplicationController
 
+  before_action :load_map, only: [:show]
+
   def index
     render json: Map.all
+  end
+
+  def show
+    render json: @map
+  end
+
+  private
+
+  def load_map
+    @map = Map.find params[:id]
   end
 
 end
--- spec/controllers/api/v1/maps_controller_spec.rb
+++ spec/controllers/api/v1/maps_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::MapsController, type: :controller do
 
   describe "GET #index" do
-    let!(:map) { FactoryGirl.create(:map, name: "treasure map") }
+    let!(:map) { create(:map, name: "treasure map") }
 
 
     before { get :index, format: :json }
@@ -17,6 +17,23 @@ RSpec.describe Api::V1::MapsController, type: :controller do
     end
 
     it { expect(response).to match_response_schema("maps") }
+
+  end
+
+  describe "GET #show" do
+    let!(:map) { create(:map, name: "treasure map") }
+
+    before { get :show, id: map.id, format: :json }
+
+    it 'responds with 200' do
+      expect(response.code).to eq '200'
+    end
+
+    it "returns the right content type" do
+      expect(response.header['Content-Type']).to eq("application/json; charset=utf-8")
+    end
+
+    it { expect(response).to match_response_schema("map") }
 
   end
 
