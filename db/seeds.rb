41c4bdb0c0b1127d3a2c3936bdd7215f0173c1a2
Lennon Manchester <le.manchester@gmail.com>
2015-10-07 21:57:41 -0300

    Add simple seeds for map and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+map = Map.create(name: "Dark Moon")
+
+routes = [
+  { origin: "A", destination: "B",  distance: 10 },
+  { origin: "B", destination: "D",  distance: 15 },
+  { origin: "A", destination: "C",  distance: 20 },
+  { origin: "C", destination: "D",  distance: 30 },
+  { origin: "B", destination: "E",  distance: 50 },
+  { origin: "D", destination: "E",  distance: 30 }
+].each do |route_hash|
+  map.routes.create(route_hash)
+end
