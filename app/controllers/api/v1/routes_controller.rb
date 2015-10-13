beea8714042d1222141bb0d3cb4d04f85901b28d
Lennon Manchester <le.manchester@gmail.com>
2015-10-12 22:32:52 -0300

    Add show method to routes controller

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::RoutesController < Api::V1::ApiController
 
   before_action :load_map, only: [:index]
+  before_action :load_route, only: [:show]
 
   def index
     render json: @map.routes
+  end
+
+  def show
+    render json: @route
   end
 
   private
@@ -12,5 +17,8 @@ class Api::V1::RoutesController < Api::V1::ApiController
     @map = Map.includes(:routes).find params[:map_id]
   end
 
+  def load_route
+    @route = Route.find params[:id]
+  end
 
 end
--- spec/controllers/api/v1/routes_controller_spec.rb
+++ spec/controllers/api/v1/routes_controller_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::RoutesController, type: :controller do
 
-describe "#index" do
-
+  describe "#index" do
     let!(:route) { create(:route) }
 
     before { get :index, map_id: route.map.id, format: :json }
@@ -11,6 +10,29 @@ describe "#index" do
     it_behaves_like 'successful response'
 
     it { expect(response).to match_response_schema("routes") }
+
+  end
+
+  describe "#show" do
+    let!(:route) { create(:route) }
+
+    context "given a successful request" do
+
+      before { get :show, id: route.id, format: :json }
+
+      it_behaves_like 'successful response'
+
+      it { expect(response).to match_response_schema("route") }
+
+    end
+
+    context "given a not found request" do
+
+      before { get :show, id: 99, format: :json }
+
+      it_behaves_like 'not found response'
+
+    end
 
   end
 
